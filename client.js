3051df99e9507f9ef88f494bb87ac3e0a25b284c
MAManaras <45687805+MAManaras@users.noreply.github.com>
2019-06-04 17:08:09 +0300

    Update client.js

--- client.js
+++ client.js
@@ -1,51 +1,54 @@
 document.getElementById('insertButton').addEventListener('click', function(){
     const xhr = new XMLHttpRequest();
     //getting the values of each textBox
-    document.getElementById('Author')
-    document.getElementById('Title')
-    document.getElementById('dropList').onchange = function(){
-        if (this.value == 'Science fiction') {
-            var optionID=document.getElementById('1');
-            alert(optionID.value);
+    var a = document.getElementById('Author').value
+    var t = document.getElementById('Title').value
+    var g = document.getElementById("dropList");
+	var gender = g.options[g.selectedIndex].value;
+    // document.getElementById('dropList').onchange = function(){
+    //     if (this.value == 'Science fiction') {
+    //         var optionID=document.getElementById('1');
+    //         var g = optionID.value;
 
-         }
-        if (this.value == 'Satire') {
-            var optionID=document.getElementById('2');
-            alert(optionID.value);
+    //      }
+    //     if (this.value == 'Satire') {
+    //         var optionID=document.getElementById('2');
+    //         var g = optionID.value;
 
-         }
-         if (this.value == 'Drama') {
-            var optionID=document.getElementById('3');
-            alert(optionID.value);
+    //      }
+    //      if (this.value == 'Drama') {
+    //         var optionID=document.getElementById('3');
+    //         var g = optionID.value;
 
-         }
-         if (this.value == 'Action and Adventure') {
-            var optionID=document.getElementById('4');
-            alert(optionID.value);
+    //      }
+    //      if (this.value == 'Action and Adventure') {
+    //         var optionID=document.getElementById('4');
+    //         var g = optionID.value;
 
-         }
-         if (this.value == 'Romance') {
-            var optionID=document.getElementById('5');
-            alert(optionID.value);
+    //      }
+    //      if (this.value == 'Romance') {
+    //         var optionID=document.getElementById('5');
+    //         var g = optionID.value;
 
-         }
-         if (this.value == 'Mystery') {
-            var optionID=document.getElementById('6');
-            alert(optionID.value);
+    //      }
+    //      if (this.value == 'Mystery') {
+    //         var optionID=document.getElementById('6');
+    //         var g = optionID.value;
 
-         }
-         if (this.value == 'Horror') {
-            var optionID=document.getElementById('7');
-            alert(optionID.value);
+    //      }
+    //      if (this.value == 'Horror') {
+    //         var optionID=document.getElementById('7');
+    //         var g = optionID.value;
 
-         }
+    //      }
          
-    }
-    document.getElementById('Price')
-    xhr.open('POST','http://localhost:8081/',true);
-    xhr.send();
+    // }
+    var p = document.getElementById('Price').value
+    xhr.open('POST','http://localhost:8081/books/',true); // true = asynchronous
+    xhr.send('{"Author":"' + a + '","Title":"' + t + '","Gender":"' + gender + '","Price":"' + p + '"}');	
     xhr.onreadystatechange = function() {
         if(xhr.readyState == 4 && xhr.status == 200){
+        	alert("information sent successfully")
             alert(xhr.responseText);
         }
     };
@@ -56,13 +59,13 @@ document.getElementById('insertButton').addEventListener('click', function(){
 
 document.getElementById('searchButton').addEventListener('click', function(){
     const xhr = new XMLHttpRequest();
-    xhr.open('GET','http://localhost:8081/',true);
+    xhr.open('GET','http://localhost:8081/books/:',true); // true = asynchronous
     xhr.send();
     xhr.onreadystatechange = function() {
         if(xhr.readyState == 4 && xhr.status == 200){
             alert(xhr.responseText);
             newObject = JSON.parse(xhr.responseText);
-            alert(newObject.); // NEED SPECIFIC STRING THERE LIKE lastName
+            //alert(newObject.); // NEED SPECIFIC STRING THERE LIKE lastName
         }
     };
 }); 
