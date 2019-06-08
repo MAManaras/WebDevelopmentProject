6ca0afd2e855f18eb67b2fb8cdd4f766d9a0582d
MAManaras <45687805+MAManaras@users.noreply.github.com>
2019-06-08 20:20:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 var express = require('express');
 var app = express();
 
+
+app.use(express.static('testhtml.html'));
+
+
+
 const cors = require('cors');
 app.use(cors());
 
 app.get('/books/:', function(req,res){
     //res.send(JSON.stringify()); //NEED OBJECT THERE 
-    res.send("hello");
+	    var mysql      = require('mysql');
+		var connection = mysql.createConnection({
+		  host     : '34.90.92.187',
+		  user     : 'root',
+		  password : 'VeM4n8mVWhAy ',
+		  database : 'books'
+		});
+		 
+		connection.connect();
+		 
+		connection.query('select * from contacts', function (error, results, fields) {
+		  if (error) throw error;
+		  console.log(JSON.stringify(results));
+		  res.send(JSON.stringify(results));
+		});
+		 
+		connection.end();
 });
 
                                                               
@@ -17,5 +38,28 @@ var server = app.listen(8081,function(){
 });
 
 app.post('/books/',function(req,res){
-	res.send()
+
+	res.send("Insert was made successfully");
 })	
+
+    var mysql      = require('mysql');
+	var connection = mysql.createConnection({
+	  acquireTimeout: 30000,
+	  host     : '34.90.92.187',
+	  user     : 'it21737',
+	  password : 'qwerty123',
+	  database: 'books',
+	});
+	 
+	connection.connect();
+	 
+	connection.query('select * from books', function (error, results, fields) {
+	  //if (error) throw error;
+	  console.log(error);
+	  // console.log(JSON.stringify(results));
+	  // res.send(JSON.stringify(results));
+	});
+	 
+	// connection.end();
+
+
